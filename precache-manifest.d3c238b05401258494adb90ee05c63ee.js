a19011f36b0d8bb6e9e627a8a0f34699f6b507ab
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-10-03 00:03:21 -0300

    Updates

---
+++ precache-manifest.d3c238b05401258494adb90ee05c63ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b524d541295d1d224373229e30cd067",
+    "url": "/my-order/index.html"
+  },
+  {
+    "revision": "7fe3195f03bfee5f92b4",
+    "url": "/my-order/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e080dc8256b482711baa",
+    "url": "/my-order/static/js/2.2246252c.chunk.js"
+  },
+  {
+    "revision": "7fe3195f03bfee5f92b4",
+    "url": "/my-order/static/js/main.4217f9da.chunk.js"
+  },
+  {
+    "revision": "1ec53bdaa2460eac94b6",
+    "url": "/my-order/static/js/runtime-main.c0cc2cb0.js"
+  }
+]);
