085ad8f84c01cd151897c8921c3168c6188ad6b6
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-10-06 16:13:44 -0300

    agrego links

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import data from "../../data/main";
 import MainCard from "../MainCard";
-import { Router } from "@reach/router";
 
 const Container = styled.div`
   display: flex;
@@ -12,11 +11,9 @@ const Container = styled.div`
 function Main() {
   return (
     <Container>
-      <Router>
-        {data.options.map(option => {
-          return <MainCard key={option.title} {...option} />;
-        })}
-      </Router>
+      {data.options.map(option => {
+        return <MainCard key={option.title} {...option} />;
+      })}
     </Container>
   );
 }
--- src/components/MainCard/index.js
+++ src/components/MainCard/index.js
@@ -5,31 +5,35 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "@reach/router";
 
-function MenuCard({ title, description }) {
+function MenuCard({ title, description, path }) {
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link to={path}>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
 
 MenuCard.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired
 };
 
 export default MenuCard;
