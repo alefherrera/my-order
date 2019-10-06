54c012ec54be21f7629734d9d5e6b2e3de0f480c
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-10-06 15:24:26 -0300

    agrego theme

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import Header from '../Header';
 import Main from '../Main';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header title="My Order">
         <Main />
-      </header>
+      </Header>
     </div>
   );
 }
