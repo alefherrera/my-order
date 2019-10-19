7ea7de50d4e6211ae026325af4d29cc552a3cb8c
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-10-19 12:07:35 -0300

    agrego preparation page

---
+++ src/containers/PreparationPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function PreparationPage(props) {
+  return <div></div>;
+}
+
+PreparationPage.propTypes = {};
+
+export default PreparationPage;
