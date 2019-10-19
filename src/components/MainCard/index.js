10e11379891253be93f5f41aa80b3ff980b72804
Alejandro Herrera <alejandro.herrera@mercadolibre.com>
2019-10-19 12:56:42 -0300

    preparation listo

--- src/components/MainCard/index.js
+++ src/components/MainCard/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Link } from '@reach/router';
 
@@ -12,10 +11,6 @@ function MenuCard({ title, description, path }) {
     <Link to={path}>
       <Card>
         <CardActionArea>
-          <CardMedia
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {title}
--- src/containers/PreparationPage/index.js
+++ src/containers/PreparationPage/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import data from '../../data/preparation';
+import ButtonContainer from '../../components/ButtonContainer';
+import { Link, Button, Typography } from '@material-ui/core';
 
-function PreparationPage(props) {
-  return <div></div>;
+const getUrl = image =>
+  //eslint-disable-next-line
+  process.env.NODE_ENV === 'development'
+    ? `/assets/${image}`
+    : `/my-order/assets/${image}`;
+
+function PreparationPage() {
+  return (
+    <div>
+      <ButtonContainer>
+        <Typography gutterBottom variant="h5" component="h2">
+          {data.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {data.description}
+        </Typography>
+        <img alt="waiting" src={getUrl(data.image)} />
+        <Link href={data.link}>
+          <Button variant="contained" color="primary">
+            {data.button}
+          </Button>
+        </Link>
+      </ButtonContainer>
+    </div>
+  );
 }
-
-PreparationPage.propTypes = {};
 
 export default PreparationPage;
